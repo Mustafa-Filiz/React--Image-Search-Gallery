43b4f3ebccb37ab239f03a5567759e57b9676b37
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-16 12:16:34 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import ResultArea from './components/ResultArea';
 import SearchBar from './components/SearchBar';
 
+const useStyles = makeStyles({
+    App: {
+        margin: "auto",
+        width: '90%',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+});
+
 function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-    </div>
-  );
+    const classes = useStyles();
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(
+                `https://pixabay.com/api/?key=23876519-c9ef1b40ba3e626025d19cec6&q=${searchTerm}&image_type=photo`
+            )
+            .then((res) => setPhotos(res.data.hits));
+    }, [searchTerm]);
+
+    return (
+        <Box className={classes.App}>
+            <SearchBar setSearchTerm={setSearchTerm} />
+            <ResultArea photos={photos} />
+        </Box>
+    );
 }
 
 export default App;
